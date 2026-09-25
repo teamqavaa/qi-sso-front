412f5fe0078e78e1630a215aa5057ce58f411d44
Ange-Pascal <angeakoumoua@gmail.com>
2026-09-25 01:04:49 +0000

    modification de l'url de l'api d'auth

--- actions/auth.ts
+++ actions/auth.ts
@@ -2,7 +2,7 @@
 
 import { cookies } from "next/headers";
 
-const DJANGO_API_URL = process.env.DJANGO_API_URL || "http://127.0.0.1:8000";
+const DJANGO_API_URL = process.env.DJANGO_API_URL || "https://qavaa-innovate-sso-zlvwvifuvq-ew.a.run.app/api/docs/#/";
 
 // Configuration unifiée et sécurisée des cookies
 const COOKIE_OPTIONS = {
